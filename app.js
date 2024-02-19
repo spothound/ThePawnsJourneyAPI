815209b34250bcd6ef9b3502e3802b8162dcfee0
Francisco Navarro Morales <francisco.morales@preply.com>
2024-02-19 18:55:42 +0100

    /

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 const puzzleController = require('./controllers/puzzleController');
 const PORT = process.env.PORT || 3000;
 
-app.use('/api', puzzleController);
+app.use('/', puzzleController);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on  http://localhost:${PORT}/api`);
+  console.log(`Server is listening on  http://localhost:${PORT}/`);
 });

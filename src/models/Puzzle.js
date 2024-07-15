60b8d9d11d0a600e1b007e3f4a4e003d4b076f12
Francisco Navarro Morales <francisco.morales@preply.com>
2024-07-15 22:44:29 +0200

    Use all puzzles

--- src/models/Puzzle.js
+++ src/models/Puzzle.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const puzzleSchema = new mongoose.Schema({
   id: { type: String, required: true },
   rating: { type: Number, required: true },
-}, { collection : 'small' });
+}, { collection : 'main' });
 
 module.exports = mongoose.model('Puzzle', puzzleSchema);
